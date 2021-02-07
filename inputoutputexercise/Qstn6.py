2f3ba9007de26c81148a359aa32a16b9b43d4c88
Ananya Biswas <itsbiswasananya@gmail.com>
2021-02-07 19:14:19 +0100

    Qstn2.py added

--- inputoutputexercise/Qstn6.py
+++ inputoutputexercise/Qstn6.py
@@ -1,14 +1,18 @@
 # write all file content into new file by skiping line 5 from following file
 
 f = open("Firstfile.txt", 'w')
-f.write('Now the file has one content')
-f.write('Now the file has one content')
-f.write('Now the file has one content')
-f.write('Now the file has one content')
-f.write('Now the file has one content')
+f.write('Now the file has one content\n')
+f.write('Now the file has one content\n')
+f.write('Now the file has one content\n')
+f.write('Now the file has one content\n')
+f.write('Now the file has one content\n')
 f.write('Another new line')
+f.close()
 
-
+# Firstfile = open('Firstfile.txt', 'r')
+# Secondfile = open('Secondfile.txt', 'a')
 with open('Firstfile.txt', 'r') as Firstfile, open('Secondfile.txt', 'a') as Secondfile:
     for line in Firstfile:
         Secondfile.write(line)
+# Firstfile.close()
+# Secondfile.close()
---
+++ inputoutputexercise/file_basic.py
@@ -0,0 +1,38 @@
+#############################################################################
+# print("Hello World")
+# print("Goodbye World")
+# ------------------------------
+# file = open("console", "a")
+# file.write("Hello World")
+# file.write("Goodbye World")
+# file.close()
+
+# fileWriter = open("hello.txt", "w")
+# fileWriter.write("Hello World\n")
+# fileWriter.write("Goodbye World\n")
+# fileWriter.close()
+
+# fileReader = open("hello.txt", "r")
+# content = fileReader.read()
+# print(content)
+# fileReader.close()
+
+# with open("hello.txt", "w") as fileWriter:
+#     fileWriter.write("Hello World\n")
+#     fileWriter.write("Goodbye World\n")
+
+# with open("hello.txt", "r") as fileReader:
+#     content = fileReader.read()
+#     print(content)
+
+
+############################################
+# fptr = open("hello.txt", "r")
+# print(fptr.readline())
+# print(fptr.readlines())
+
+# for line in fptr:
+#     print(line)
+
+# for line in fptr.readlines():
+#     print(line)
